9ca5fcfba63402282a80062ebf6a299766e8ce47
arifuzzaman31 <arifuzzaman.rb@gmail.com>
2025-07-09 09:58:00 +0600

    add for merge

---
+++ app/Models/ServiceItem.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceItem extends Model
+{
+    protected $fillable = [
+        'service_id',
+        'yarn_count_id',
+        'quantity',
+        'unit_attr_id',
+        'unit_price',
+        'extra_quantity',
+        'extra_quantity_price',
+        'color_id',
+        'gross_weight',
+        'net_weight',
+        'weight_attr_id',
+        'bobin',
+        'remark'
+    ];
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function yarnCount()
+    {
+        return $this->belongsTo(Yarn::class);
+    }
+
+    public function unitAttr()
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+
+    public function color()
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+
+    public function weightAttr()
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+}
